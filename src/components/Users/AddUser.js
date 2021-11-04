a7fc3d354ba87b511d3a6e492f72408ea3dde567
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-04 13:53:56 +0200

    add Card component and some css styles

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
+import Card from '../UI/Card.js'
 
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
     const addUserHandler = (event) => {
@@ -6,13 +8,15 @@ const AddUser = (props) => {
     };
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="">Username</label>
-            <input id="userName" type="text" />
-            <label htmlFor="">Age (Years)</label>
-            <input id="age" type="text" />
-            <button type="submit">Add User</button>
-        </form>
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="">Username</label>
+                <input id="userName" type="text" />
+                <label htmlFor="">Age (Years)</label>
+                <input id="age" type="text" />
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     )
 };
 
